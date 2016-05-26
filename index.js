e753fd851c242f319ce7d1bbcbad590dded8d41a
helio-frota <00hf11@gmail.com>
2016-05-26 00:49:20 -0300

    issue #2

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const term = require('./lib/args-handler').grabArgs(process.argv);
-
-if (term) {
-  require('./lib/ud-cli')(term)
+if (process.argv.length === 3) {
+  require('./lib/ud-cli')(process.argv[2])
     .then(data => console.log(JSON.parse(data).list[0].definition))
     .catch(error => console.log(error));
+} else {
+  console.log('Usage: node index search_term_here');
 }
