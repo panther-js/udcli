6431431b3d11669183abc2b2c6d9b178ce2476bd
helio-frota <00hf11@gmail.com>
2016-05-26 00:57:35 -0300

    issue #2

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # udcli
+
 A nodejs urban dictionary cli.
 
 #### Example
 
-    node index --t lgtm
+    node index lgtm
     
-    An acronym for "Looks Good To Me", often used as a quick response after reviewing someone's essay, code, or design document.
+    An acronym for "Looks Good To Me", often used as a quick response after reviewing someone's essay, 
+    code, or design document.
--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 'use strict';
 
 if (process.argv.length === 3) {
-  require('./lib/ud-cli')(process.argv[2])
-    .then(data => console.log(JSON.parse(data).list[0].definition))
-    .catch(error => console.log(error));
+
+  const url = 'http://api.urbandictionary.com/v0/define?term=' + process.argv[2];
+
+  const udcli = () => {
+    return new Promise((reso, rej) => {
+      require('http').get(url, (resp) => {
+        const body = [];
+        resp.on('data', d => body.push(d));
+        resp.on('end', () => reso(body.join('')));
+      }).on('error', (error) => rej(error));
+    });
+  };
+
+  udcli()
+    .then(d => console.log(JSON.parse(d).list[0].definition))
+    .catch(e => console.log(e));
+
 } else {
   console.log('Usage: node index search_term_here');
 }
