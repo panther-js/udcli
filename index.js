35ef2fcaf9a95a1464de67df918e99b013aadee5
helio-frota <00hf11@gmail.com>
2016-06-03 13:01:23 -0300

    using roi lib

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 'use strict';
 
+let roi = require('roi');
+
 if (process.argv.length === 3) {
 
-  new Promise((reso, rej) => {
-    require('http').get('http://api.urbandictionary.com/v0/define?term=' + process.argv[2], resp => {
-      let body = [];
-      resp.on('data', d => body.push(d));
-      resp.on('end', () => reso(body));
-    }).on('error', e => rej(e));
-  }).then(d => console.log(JSON.parse(d).list[0].definition)).catch(e => console.log(e));
+  let url = '/v0/define?term=' + process.argv[2];
+
+  let opts = { host: 'api.urbandictionary.com' };
+
+  roi(opts).get(url)
+    .then(d => console.log(d.list[0].definition))
+    .catch(e => console.log(e));
 
 } else {
   console.log('Usage: node index search_term_here');
