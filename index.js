227c6e1cae39aed94a7a4a679d4e72aff2b04778
helio-frota <00hf11@gmail.com>
2016-08-24 15:05:27 -0300

    This closes #11

--- index.js
+++ index.js
@@ -3,15 +3,13 @@
 const roi = require('roi');
 
 if (process.argv.length === 3) {
-
   const options = {
     'endpoint': 'http://api.urbandictionary.com/v0/define?term=' + process.argv[2]
   };
-  
+
   roi.get(options)
     .then(x => console.log(JSON.parse(x.body).list[0].definition))
     .catch(e => console.log(e));
-
 } else {
   console.log('Usage: node index search_term_here');
 }
