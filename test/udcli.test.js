fa458bc7f4a990e1448b75fe80383cd5ca212c5c
Helio Frota <00hf11@gmail.com>
2017-12-22 14:35:39 -0300

    using roi 0.17.0 and jest

--- test/udcli.test.js
+++ test/udcli.test.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const test = require('tape');
 const roi = require('roi');
 
-test('lgtm', (t) => {
+test('lgtm', () => {
+  expect.assertions(1);
   const expected = 'An acronym for "Looks Good To Me", often used as a quick response after reviewing someone\'s essay, code, or design document.';
-  roi.get({ endpoint: 'http://api.urbandictionary.com/v0/define?term=lgtm' })
+  return roi.get('http://api.urbandictionary.com/v0/define?term=lgtm')
     .then(response => {
-      t.equal(JSON.parse(response.body).list[0].definition, expected);
-      t.end();
+      expect(JSON.parse(response.body).list[0].definition).toBe(expected);
     }).catch(e => console.log(e));
 });
