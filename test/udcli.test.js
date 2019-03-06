b515483751ad006da52ee39a4879d09dc5c6c9d6
Felipe Martins <felipewmartins@gmail.com>
2019-03-06 10:46:00 -0300

    Fixed test.

--- test/udcli.test.js
+++ test/udcli.test.js
@@ -4,7 +4,7 @@ const roi = require('roi');
 
 test('lgtm', () => {
   expect.assertions(1);
-  const expected = 'An acronym for "Looks Good To Me", often used as a quick response after reviewing someone\'s essay, code, or design document.';
+  const expected = 'An acronym for "[Looks Good] To Me", often used as a quick response after reviewing someone\'s [essay], code, or design [document].';
   return roi.get('http://api.urbandictionary.com/v0/define?term=lgtm')
     .then(response => {
       expect(JSON.parse(response.body).list[0].definition).toBe(expected);
